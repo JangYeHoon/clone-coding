97d0abe17c2436108f70f36aaa6a7501cc6815d1
root <root@test-server-1.novalocal>
2023-11-23 09:24:09 +0000

    Add movie rank weekly & 10 years ago

--- cinerank/rank/apis.py
+++ cinerank/rank/apis.py
@@ -6,7 +6,12 @@ class RankApi:
     def __init__(self):
         self.api_key = settings.SECRET_KEY
     
-    def get_day_rank(self, date):
+    def get_day_ranks(self, date):
         response = requests.get("http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key="
                                 + self.api_key + "&targetDt=" + date)
         return response.json()['boxOfficeResult']['dailyBoxOfficeList']
+ 
+    def get_weekly_ranks(self, date):
+        response = requests.get("http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key="
+                                + self.api_key + "&targetDt=" + date)
+        return response.json()['boxOfficeResult']['weeklyBoxOfficeList']
--- cinerank/rank/views.py
+++ cinerank/rank/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from rank.apis import RankApi
+from datetime import datetime, timedelta
 
 def ranks(request):
     api = RankApi()
-    daily_ranks = api.get_day_rank("20231025")
-    context = {'daily_ranks': daily_ranks}
+
+    yesterday_date = datetime.today() - timedelta(1)
+    daily_ranks = api.get_day_ranks(yesterday_date.strftime("%Y%m%d"))
+
+    year_ago_date = datetime.today() - timedelta(365)
+    year_ago_ranks = api.get_day_ranks(year_ago_date.strftime("%Y%m%d"))
+
+    weekly_date = datetiem.today() - timedelta(7)
+    weekly_ranks = api.get_weekly_ranks(weekly_date.strftime("%Y%m%d"))
+
+    context = {
+        'daily_ranks': daily_ranks,
+        'year_ago_ranks': year_ago_ranks,
+        'weekly_ranks': weekly_ranks,
+    }
     return render(request, 'index.html', context)
