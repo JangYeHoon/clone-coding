8b72608a5526c898859f31b6256e1a185e5bcc7d
JangYeHoon <jkjk128jkjk@gmail.com>
2024-02-10 22:54:58 +0900

    update

--- cinerank/users/views.py
+++ cinerank/users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        print('post')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=email, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('/rank/ranks')
         return render(request, 'login.html')
     else:
         print('just')
         return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/rank/ranks')
