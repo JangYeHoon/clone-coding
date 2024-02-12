6d3161d9e8df358438fb95e2e4c89bc65a4140e4
JangYeHoon <jkjk128jkjk@gmail.com>
2024-02-12 22:09:40 +0900

    add signup

--- cinerank/users/views.py
+++ cinerank/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from users.models import User
 
 # Create your views here.
 def login_view(request):
@@ -13,10 +14,26 @@ def login_view(request):
             login(request, user)
             return redirect('/rank/ranks')
         return render(request, 'login.html')
-    else:
-        print('just')
-        return render(request, 'login.html')
+    return render(request, 'login.html')
 
 def logout_view(request):
     logout(request)
     return redirect('/rank/ranks')
+
+def signup_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password1 = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if User.objects.filter(username=email).exists():
+            return render(request, 'signup.html')
+        if password1 != password2:
+            return render(request, 'signup.html')
+        user = User.objects.create_user(
+            username=email,
+            password=password1,
+        )
+        login(request, user)
+        return redirect('/rank/ranks')
+    return render(request, 'signup.html')
