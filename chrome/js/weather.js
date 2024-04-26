3708dde11d832e886873620a3d43b409eccb3d8f
JangYeHoon <jkjk128jkjk@gmail.com>
2024-04-26 20:06:56 +0900

    finish chrome js app

---
+++ chrome/js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "f03aa2f9d190785bb159a8e2f0e047e4";
+
+function onGeoOk(positon) {
+    const lat = positon.coords.latitude;
+    const lon = positon.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
